65df373fa79439b13413383f29a1084ed3a847d9
JuliansRecent <juliansrecent@gmail.com>
2023-01-25 19:22:08 -0800

    Module complete. Create a conditional statement that takes an int named 'speed.' If the speed is greater than 120, print 'Speeding ticket.'

--- src/main/java/SpeedingTicket.java
+++ src/main/java/SpeedingTicket.java
@@ -6,6 +6,13 @@ public class SpeedingTicket {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here. 
+        // Write your program here.
+        // Prompt the user to input a speed from the console
+        System.out.println("Give speed:");
+        int speed = Integer.valueOf(scanner.nextLine());
+        // If the given speed is over 120 print "Speeding ticket!"
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
     }
 }
